70cf19444ab1cbb5651d6d8f0ba977a95e183582
Mohamed Allam <mohamedallam.tu@gmail.com>
2022-10-05 20:59:21 +0200

    Add more folder structure

--- server/Middleware.js
+++ server/Middleware.js
@@ -1,6 +1,21 @@
-function include(file, props) {
-  console.log(props = {}, "Loading: " + file)
-  var template = HtmlService.createTemplateFromFile(file);
-  Object.assign(template, props);
-  return template.evaluate().getContent()
-}
+; (function (root, factory) {
+  root.MW = factory()
+})(this, function () {
+
+  function include(file, props) {
+    var template = HtmlService.createTemplateFromFile(file);
+    Object.assign(template, props);
+    return template.evaluate().getContent()
+  }
+
+  function render(file, props = {}) {
+    const fileTemplate = HtmlService.createTemplateFromFile(file);
+    Object.assign(fileTemplate, props);
+    return fileTemplate.evaluate().setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+  }
+
+  return {
+    include,
+    render
+  }
+})
--- server/Server.js
+++ server/Server.js
@@ -1,4 +1,11 @@
-function doGet(e) {
-  const indexTemplate = HtmlService.createTemplateFromFile('index');
-  return indexTemplate.evaluate();
-}
+var app   = new Gexpress.App()
+
+app.get( /.*/, function(req,res,next){ // default to homepage
+  res.set('content-type','text/html')
+  res.send( MW.render('client/index').getContent() )
+  res.end()
+})
+
+// this hooks Gexpress into appscript 
+function doGet(e) { return app.doGet(e)  }
+function doPost(e){ return app.doPost(e) }
